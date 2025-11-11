27f558c9155178a650fc515936a9f4c0d1f6234d
Marco <formante.marco@gmail.com>
2025-11-11 15:33:28 +0000

    create Book model with getters and setters

---
+++ models/Book.php
@@ -0,0 +1,115 @@
+<?php 
+
+class Book extends AbstractModel{
+    private string $title;
+    private int $author_id;
+    private string $image;
+    private string $description;
+    private bool $status;
+
+    
+
+    /**
+     * Get the value of title
+     */ 
+    public function getTitle()
+    {
+        return $this->title;
+    }
+
+    /**
+     * Set the value of title
+     *
+     * @return  self
+     */ 
+    public function setTitle($title)
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+ 
+
+    /**
+     * Get the value of image
+     */ 
+    public function getImage()
+    {
+        return $this->image;
+    }
+
+    /**
+     * Set the value of image
+     *
+     * @return  self
+     */ 
+    public function setImage($image)
+    {
+        $this->image = $image;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of description
+     */ 
+    public function getDescription()
+    {
+        return $this->description;
+    }
+
+    /**
+     * Set the value of description
+     *
+     * @return  self
+     */ 
+    public function setDescription($description)
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of status
+     */ 
+    public function getStatus()
+    {
+        return $this->status;
+    }
+
+    /**
+     * Set the value of status
+     *
+     * @return  self
+     */ 
+    public function setStatus($status)
+    {
+        $this->status = $status;
+
+        return $this;
+    }
+
+
+
+    /**
+     * Get the value of author_id
+     */ 
+    public function getAuthor_id()
+    {
+        return $this->author_id;
+    }
+
+    /**
+     * Set the value of author_id
+     *
+     * @return  self
+     */ 
+    public function setAuthor_id($author_id)
+    {
+        $this->author_id = $author_id;
+
+        return $this;
+    }
+}
