8ed1caf2a468597c0e55a1c0fec4fc9eb2240b57
Marco <formante.marco@gmail.com>
2025-11-14 12:48:31 +0000

    create AuthorManager with getAuthor and create or update author functions

---
+++ models/AuthorManager.php
@@ -0,0 +1,48 @@
+<?php 
+
+class AuthorManager extends AbstractEntityManager{
+
+    /**
+     * Get Author by ID
+     * @param int $id
+     * @return Author|bool
+    */
+    public function getAuthor(int $id):Author|false
+    {
+        $sql = "SELECT id, name, surname, pseudo FROM authors WHERE id = :id";
+
+        $stmt = $this->db->query($sql,['id' => $id]);
+        $author = $stmt->fetch();
+
+        if (!$author) {
+            return false;
+        }
+        return new Author($author);
+    }
+
+
+    /**
+     *Create or Update an Author
+     *@param Author
+     *@return bool stmt->rowCount()
+    */
+    public function createOrUpdateAuthor(Author $author):bool
+    {
+        $params = [
+            'name'    => $author->getName(),
+            'surname' => $author->getSurname(),
+            'pseudo'  => $author->getPseudo()
+        ];
+
+        if ($author->getId() !== -1) {
+            $sql = "UPDATE authors SET name = :name, surname = :surname, pseudo = :pseudo WHERE id = :id";
+            $params['id'] = $author->getId();
+        }else{
+            $sql = "INSERT INTO authors(name,surname,pseudo) VALUES(:name,:surname,:pseudo)";
+        }
+
+        $stmt = $this->db->query($sql,$params);
+        
+        return $stmt->rowCount();
+    }
+}
