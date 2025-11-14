994f70fe422e55fdb7e5c6f5da75750d2b48c0db
Marco <formante.marco@gmail.com>
2025-11-14 12:47:34 +0000

    create User Manager with getUser and create or update user functions

--- models/UserManager.php
+++ models/UserManager.php
@@ -1,17 +1,51 @@
 <?php 
 
 class UserManager extends AbstractEntityManager{
+
+    /**
+     * Get User by ID
+     * @param int $id
+     * @return Author|bool
+    */
+    public function getUser(int $id):User|false
+    {
+        $sql = "SELECT id, email, password, pseudo FROM users WHERE id = :id";
+
+        $stmt = $this->db->query($sql,['id' => $id]);
+        $user = $stmt->fetch();
+
+        if (!$user) {
+            return false;
+        }
+        return new User($user);
+    }
     
-    public function createUser(User $user){
-        $sql = "INSERT INTO users(email,password,pseudo) VALUES(:email,:password,:pseudo)";
+    /**
+    * Create or Update a User
+    * @param User
+    * @return bool stmt->rowCount()
+    */
+    public function createOrUpdateUser(User $user):bool
+    {
         $password = password_hash($user->getPassword(),PASSWORD_BCRYPT, ['cost' => 13]);
-        $this->db->query($sql,
-            [
-                'email' => $user->getEmail(),
-                'password' => $password ,
-                'pseudo' => $user->getPseudo()
-            ]
-        );
+
+        $params = [
+            'email'    => $user->getEmail(),
+            'password' => $password,
+            'pseudo'   => $user->getPseudo()
+        ];
+
+        // Check if it isn't a new User
+        if ($user->getId() !== -1) {
+              $sql = 'UPDATE users SET email = :email, password = :password, pseudo = :pseudo WHERE id = :id';
+              $params['id'] = $user->getId();
+        }else{
+            $sql = "INSERT INTO users(email,password,pseudo) VALUES(:email,:password,:pseudo)";
+        }
+        
+        $stmt = $this->db->query($sql,$params);
+
+        return $stmt->rowCount();
     }
 
 }
