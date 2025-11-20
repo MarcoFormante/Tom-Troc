e8e052ac719d09b5779b62b6406865354d5d02c1
Marco <formante.marco@gmail.com>
2025-11-20 17:47:11 +0000

    delete author table, add foreign keys in books and chatrooms

--- tomtroc.sql
+++ tomtroc.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Creato il: Nov 15, 2025 alle 00:04
+-- Creato il: Nov 20, 2025 alle 18:45
 -- Versione del server: 10.4.32-MariaDB
 -- Versione PHP: 8.2.12
 
@@ -24,36 +24,39 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Struttura della tabella `authors`
---
-
-CREATE TABLE `authors` (
-  `id` int(11) NOT NULL,
-  `name` varchar(255) NOT NULL,
-  `surname` varchar(255) NOT NULL,
-  `pseudo` varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Dump dei dati per la tabella `authors`
---
-
-INSERT INTO `authors` (`id`, `name`, `surname`, `pseudo`) VALUES
-(1, 'asd', 'asd', 'asd');
-
--- --------------------------------------------------------
-
---
 -- Struttura della tabella `books`
 --
 
 CREATE TABLE `books` (
   `id` int(11) NOT NULL,
   `title` varchar(255) NOT NULL,
-  `author_id` int(11) NOT NULL,
   `image` varchar(255) NOT NULL,
   `description` text NOT NULL,
-  `status` tinyint(4) NOT NULL DEFAULT 1
+  `status` tinyint(4) NOT NULL DEFAULT 1,
+  `created_at` datetime NOT NULL DEFAULT current_timestamp(),
+  `author` varchar(255) NOT NULL,
+  `sold_by` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dump dei dati per la tabella `books`
+--
+
+INSERT INTO `books` (`id`, `title`, `image`, `description`, `status`, `created_at`, `author`, `sold_by`) VALUES
+(44, 'asdasd', 'asdsad', 'asdasdasd', 1, '2025-11-20 17:34:43', 'asd asdasd', 126),
+(45, 'asdasd', 'asdasda', 'asdasd', 1, '2025-11-20 17:34:52', 'asdasd', 126);
+
+-- --------------------------------------------------------
+
+--
+-- Struttura della tabella `chatrooms`
+--
+
+CREATE TABLE `chatrooms` (
+  `id` varchar(255) NOT NULL,
+  `created_at` datetime NOT NULL DEFAULT current_timestamp(),
+  `user_one_id` int(11) NOT NULL,
+  `user_two_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
@@ -65,10 +68,24 @@ CREATE TABLE `books` (
 CREATE TABLE `messages` (
   `id` int(11) NOT NULL,
   `chatroom_id` varchar(255) NOT NULL,
-  `user_one_id` int(11) NOT NULL,
-  `user_two_id` int(11) NOT NULL,
   `content` text NOT NULL,
-  `sent_at` datetime NOT NULL DEFAULT current_timestamp()
+  `sent_at` datetime NOT NULL DEFAULT current_timestamp(),
+  `sent_by_user_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Struttura della tabella `notifications`
+--
+
+CREATE TABLE `notifications` (
+  `id` int(11) NOT NULL,
+  `id_message` int(11) NOT NULL,
+  `from_user_id` int(11) NOT NULL,
+  `to_user_id` int(11) NOT NULL,
+  `last_message_id_viewed` int(11) NOT NULL,
+  `chatroom_id` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
@@ -82,44 +99,54 @@ CREATE TABLE `users` (
   `pseudo` varchar(255) NOT NULL,
   `password` varchar(255) NOT NULL,
   `signup_date` datetime NOT NULL DEFAULT current_timestamp(),
-  `id` int(11) NOT NULL
+  `id` int(11) NOT NULL,
+  `profile_image` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dump dei dati per la tabella `users`
 --
 
-INSERT INTO `users` (`email`, `pseudo`, `password`, `signup_date`, `id`) VALUES
-('asdss', 'asd@asd', '$2y$13$EKe2jEvXNEk0T39.Cnai2.1w1PdwIpxsZ53R2EYB3mQaeCmPh5BBG', '2025-11-14 11:41:54', 50),
-('asasdd@asd', 'asssd@asd', '$2y$13$/v5r7224TFKb3y6GgIAacOCk6JE6bLIMT4TzPUW4tyCYC3heMSSWe', '2025-11-14 11:43:17', 54),
-('asdsasdaasdsds', 'asdsaasdsdasds', '$2y$13$vmB3fPylLsoRsUAX4AtmAuWznflANz8CkyRILdqIOecUlxB0qFUeq', '2025-11-14 11:56:39', 55),
-('addsd', 'asddd', '$2y$13$OMhOY1MmRDmVGZHmhB8Qe.YIaEiRA9bj6Wq5cAumU445v2FNBd8.S', '2025-11-14 12:39:22', 56),
-('addsdsd', 'asddsdd', '$2y$13$4k9FaIH6j7KOei7kvQR3RukDnucc96kLDqYUV6i6TvuK3RzcVAxX.', '2025-11-14 12:40:32', 61);
+INSERT INTO `users` (`email`, `pseudo`, `password`, `signup_date`, `id`, `profile_image`) VALUES
+('hvmsdacjbtoyheprgl@gmail.com', 'hvmsdacjbtoyheprgl', 'pass-691f4711e103ehvmsdacjbtoyheprgl', '2025-11-20 16:51:29', 126, 'img-691f4711e1040.jpg'),
+('mrjhbonvtywf@gmail.com', 'mrjhbonvtywf', 'pass-691f4724e5d9amrjhbonvtywf', '2025-11-20 16:51:48', 127, 'img-691f4724e5d9c.jpg');
 
 --
 -- Indici per le tabelle scaricate
 --
 
 --
--- Indici per le tabelle `authors`
---
-ALTER TABLE `authors`
-  ADD PRIMARY KEY (`id`);
-
---
 -- Indici per le tabelle `books`
 --
 ALTER TABLE `books`
   ADD PRIMARY KEY (`id`),
-  ADD KEY `author_id` (`author_id`);
+  ADD KEY `sold_by` (`sold_by`);
+
+--
+-- Indici per le tabelle `chatrooms`
+--
+ALTER TABLE `chatrooms`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `user_one_id` (`user_one_id`),
+  ADD KEY `user_two_id` (`user_two_id`);
 
 --
 -- Indici per le tabelle `messages`
 --
 ALTER TABLE `messages`
   ADD PRIMARY KEY (`id`),
-  ADD KEY `user_one_id` (`user_one_id`),
-  ADD KEY `user_two_id` (`user_two_id`);
+  ADD KEY `chatroom_id` (`chatroom_id`),
+  ADD KEY `sent_by_user_id` (`sent_by_user_id`);
+
+--
+-- Indici per le tabelle `notifications`
+--
+ALTER TABLE `notifications`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `id_message` (`id_message`),
+  ADD KEY `chatroom_id` (`chatroom_id`),
+  ADD KEY `from_user_id` (`from_user_id`),
+  ADD KEY `to_user_id` (`to_user_id`);
 
 --
 -- Indici per le tabelle `users`
@@ -134,28 +161,28 @@ ALTER TABLE `users`
 --
 
 --
--- AUTO_INCREMENT per la tabella `authors`
---
-ALTER TABLE `authors`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
-
---
 -- AUTO_INCREMENT per la tabella `books`
 --
 ALTER TABLE `books`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=46;
 
 --
 -- AUTO_INCREMENT per la tabella `messages`
 --
 ALTER TABLE `messages`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+
+--
+-- AUTO_INCREMENT per la tabella `notifications`
+--
+ALTER TABLE `notifications`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT per la tabella `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=62;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=128;
 
 --
 -- Limiti per le tabelle scaricate
@@ -165,14 +192,30 @@ ALTER TABLE `users`
 -- Limiti per la tabella `books`
 --
 ALTER TABLE `books`
-  ADD CONSTRAINT `books_ibfk_1` FOREIGN KEY (`author_id`) REFERENCES `authors` (`id`) ON DELETE CASCADE;
+  ADD CONSTRAINT `books_ibfk_1` FOREIGN KEY (`sold_by`) REFERENCES `users` (`id`);
+
+--
+-- Limiti per la tabella `chatrooms`
+--
+ALTER TABLE `chatrooms`
+  ADD CONSTRAINT `chatrooms_ibfk_1` FOREIGN KEY (`user_one_id`) REFERENCES `users` (`id`),
+  ADD CONSTRAINT `chatrooms_ibfk_2` FOREIGN KEY (`user_two_id`) REFERENCES `users` (`id`);
 
 --
 -- Limiti per la tabella `messages`
 --
 ALTER TABLE `messages`
-  ADD CONSTRAINT `messages_ibfk_1` FOREIGN KEY (`user_one_id`) REFERENCES `users` (`id`),
-  ADD CONSTRAINT `messages_ibfk_2` FOREIGN KEY (`user_two_id`) REFERENCES `users` (`id`);
+  ADD CONSTRAINT `messages_ibfk_1` FOREIGN KEY (`chatroom_id`) REFERENCES `chatrooms` (`id`),
+  ADD CONSTRAINT `messages_ibfk_2` FOREIGN KEY (`sent_by_user_id`) REFERENCES `users` (`id`);
+
+--
+-- Limiti per la tabella `notifications`
+--
+ALTER TABLE `notifications`
+  ADD CONSTRAINT `notifications_ibfk_1` FOREIGN KEY (`id_message`) REFERENCES `messages` (`id`),
+  ADD CONSTRAINT `notifications_ibfk_2` FOREIGN KEY (`chatroom_id`) REFERENCES `chatrooms` (`id`),
+  ADD CONSTRAINT `notifications_ibfk_3` FOREIGN KEY (`from_user_id`) REFERENCES `users` (`id`),
+  ADD CONSTRAINT `notifications_ibfk_4` FOREIGN KEY (`to_user_id`) REFERENCES `users` (`id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
