c5711ffe2c4896621bb7237ac354f041850aa92d
Marco <formante.marco@gmail.com>
2025-11-21 10:13:32 +0000

    create function to Check active Route and add className to nav item

--- services/Utils.php
+++ services/Utils.php
@@ -2,9 +2,10 @@
 
 class Utils{
     
-    public static function request(string $requestValue,string $defaultValue){
+    public static function request(string $requestValue,string $defaultValue = ""){
         return $_REQUEST[$requestValue] ?? $defaultValue;
     }
+
 
      public static function handleRoute(string $route):void{
         switch($route){
@@ -16,6 +17,17 @@ class Utils{
                 throw new Exception("La page demandée d'existe pas", 404);
             break;
         }
+    }
+
+    /**
+    * Check active Route and add className to nav item
+    * @param string $route
+    * @return string $className
+    */
+    public static function checkActiveRoute(string $route){
+        $request = self::request("route");
+
+        return $request === $route ? "active" : "";
     }
    
 }
