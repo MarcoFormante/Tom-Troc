fbc61f9ecd5bb213efd68e83261520d9d9368dbe
Marco <formante.marco@gmail.com>
2025-11-24 23:04:48 +0000

    create books page

---
+++ views/templates/books.php
@@ -0,0 +1,26 @@
+
+<div id="books">
+     <div id="books-heading-container" class="flex-sb-c">
+        <h1>Nos livres à l’échange</h1>
+        <form id="search-book" action="index.php?route=/nos-livre-a-lechange" method="POST">
+            <label for="search">Rechercher un livre</label>
+            <button type="submit"></button>
+            <input  type="search" maxlength="60" name="searchValue" id="search" placeholder="Rechercher un livre" value="<?= htmlspecialchars($value ?? "")  ?>"/>
+        </form>
+     </div>
+
+    <?php if(isset($error)): ?>
+        <div>
+            <p class="error-text"><?= htmlspecialchars($error) ?></p>
+        </div>
+    <?php endif?>
+     
+     <?php if(isset($books)): ?>
+        <ul class="book-list-container">
+            <?php foreach ($books as $book){ ?>
+                <?php include COMPONENTS_PATH . "book.php"?>
+            <?php } ?>
+        </ul>
+    <?php endif?>
+    
+</div>
