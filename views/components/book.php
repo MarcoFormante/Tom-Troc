9ce08cb4b03fdae80747f10355738ad860eb4bff
Marco <formante.marco@gmail.com>
2025-11-24 23:09:02 +0000

    add book instance instead of simple array

--- views/components/book.php
+++ views/components/book.php
@@ -1,12 +1,12 @@
 <li>
-    <a class="book-card" href="?route=/detail&id=<?= htmlspecialchars($book['id']) ?>">
+    <a class="book-card" href="?route=/detail&id=<?= htmlspecialchars($book->getId()) ?>">
         <div class="book-card-white-container">
             <div class="flex-col-gap-16">
-                <img width="200" height="200" src="<?= IMAGES_PATH . htmlspecialchars($book['image']) ?>" alt="<?=  htmlspecialchars($book['title']) ?>">
-                <h4 class="book-card-title"><?= htmlspecialchars($book['title']) ?></h4>
-                <span class="book-card-author"><?= htmlspecialchars($book['author'])?></span>
+                <img width="200" height="200" src="<?= IMAGES_PATH . htmlspecialchars($book->getImage()) ?>" alt="<?=  htmlspecialchars($book->getTitle()) ?>">
+                <h4 class="book-card-title"><?= htmlspecialchars($book->getTitle()) ?></h4>
+                <span class="book-card-author"><?= htmlspecialchars($book->getAuthor())?></span>
             </div>
-            <p class="book-card-soldby">Vendu par : <?= htmlspecialchars($book['pseudo'])?></p>
+            <p class="book-card-soldby">Vendu par : <?= htmlspecialchars($book->getPseudo())?></p>
         </div>
     </a>
 </li>
