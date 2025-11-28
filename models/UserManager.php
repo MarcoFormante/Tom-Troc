df8df2738198c16dc0377da3a72d03589926b275
Marco <formante.marco@gmail.com>
2025-11-28 12:52:36 +0000

    create method getuserInfo

--- models/UserManager.php
+++ models/UserManager.php
@@ -24,22 +24,27 @@ class UserManager extends AbstractEntityManager{
     * Create or Update a User
     * @param User
     * @return bool stmt->rowCount()
-    */
+    */  
     public function createOrUpdateUser(User $user):bool
     {
-        $password = password_hash($user->getPassword(),PASSWORD_BCRYPT, ['cost' => 13]);
+        $sql = 'UPDATE users SET email = :email, pseudo = :pseudo, profile_image = :image WHERE id = :id';
 
-        $params = [
-            'email'    => $user->getEmail(),
-            'password' => $password,
-            'pseudo'   => $user->getPseudo(),
-            'image'   => $user->getProfileImage()
+         $params = [
+                'email'    => $user->getEmail(),
+                'pseudo'   => $user->getPseudo(),
+                'image'   => $user->getProfileImage()
         ];
+
+         if ($password = $user->getPassword()) {
+                $params['password'] = password_hash($password,PASSWORD_BCRYPT, ['cost' => 13]);
+                $sql = 'UPDATE users SET email = :email, password = :password, pseudo = :pseudo, profile_image = :image WHERE id = :id';
+            }
+
 
         // Check if it isn't a new User
         if ($user->getId() !== -1) {
-              $sql = 'UPDATE users SET email = :email, password = :password, pseudo = :pseudo, profile_image = :image WHERE id = :id';
-              $params['id'] = $user->getId();
+
+            $params['id'] = $user->getId();
         }else{
             $sql = "INSERT INTO users(email,password,pseudo,profile_image) VALUES(:email,:password,:pseudo,:image)";
         }
@@ -48,5 +53,21 @@ class UserManager extends AbstractEntityManager{
 
         return $stmt->rowCount();
     }
+
+
+
+
+    public function getUserInfo(int $id){
+        $sql = "SELECT id, email, password, pseudo, profile_image, signup_date FROM users WHERE id = :id";
+        $stmt = $this->db->query($sql,['id' => $id]);
+
+        $userData = $stmt->fetch();
+        $user = new User($userData);
+
+        return $user;
+    }
+
+
+
 
 }
