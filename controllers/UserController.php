1877c02acff3c43efac928eac3ae38743e2cdf1e
Marco <formante.marco@gmail.com>
2025-12-06 11:54:35 +0000

    create register and login methods with csrf and error handling; createOrUpdateUser handling new user or update user data

--- controllers/UserController.php
+++ controllers/UserController.php
@@ -4,66 +4,69 @@ class UserController extends AbstractController
 {
     public function userProfile(bool $isOwner,?array $errors = [])
     {
-        // $env = parse_ini_file("../.env");
-        // var_dump($env['APP_SECRET']);
-       
+        $csrf = Utils::generateCSRF("csrf-profile"); 
         $userManager = new UserManager(); 
-        $userId = Utils::request('userId', 0);
+        $userId = Utils::request('userId', -1);
 
-        /**VERIFICARE l'USER */
+            /**Verification de l'USER */
             if ($isOwner === true) {
-                $userId = 151;
-            }
-
-            if (!$userId) {
-                throw new Exception("Ce Profile n'existe pas", 404);
+                $decodedUserData = Utils::validateJWT();
+                if (!$decodedUserData) {
+                    $this->redirect("index.php?route=/connection");
+                }
+                $userId = $decodedUserData['id'];
             }
 
             $user = $userManager->getUserInfo($userId);
-            $books = $user->getBooks();
+
+            if (!$user) {
+                $this->redirect("index.php?route=/connection");
+            }
+            $books = $user->getBooks($isOwner);
 
             $title = $isOwner ? "Mon Compte" : "Compte de " . $user->getPseudo();
 
-            $this->render("profile", ['user' => $user, 'books' => $books, 'isOwner' => $isOwner,'errors' => $errors], $title);
+            $this->render("profile", ['user' => $user, 'books' => $books, 'isOwner' => $isOwner,'errors' => $errors, 'csrf' => $csrf], $title);
     }
 
 
 
-    public function updateUser()
+    public function createOrUpdateUser()
     {   
-
         $userId = Utils::request('userId',-1);
         $email = Utils::request('email');
         $password = Utils::request('password',"");
         $pseudo = Utils::request('pseudo');
-        $image =  $_FILES['profileImage'];
-        $lastImage =  Utils::request('lastProfileImage');
-          
+        $lastImage =  Utils::request('lastProfileImage',"");
+        $csrf = Utils::request('csrf',"");
+        $image = [];
         $errors = [];
+
+
         if (!filter_var($email,FILTER_VALIDATE_EMAIL) || !$email ||strlen($email) > 60) {
             $errors['email'] = "Veuillez entrer une adresse email valide, max 60 caractères";
         }
 
-        if ($password && (strlen($password) < 5 || strlen($password) > 60) ) {
-            $errors['password'] = "Le mot de passe doit contenir au moins 5 caractères et max 60, une lettre majuscule, un chiffre et un caractère spécial";
+        if ($password && (strlen($password) < 5 || strlen($password) > 60) )  {
+            $errors['password'] = "Le mot de passe doit contenir au moins 5 caractères et un max 60, une lettre majuscule, un chiffre et un caractère spécial";
 
-        }else if($password && (strlen($password) >= 5 && strlen($password) <= 60)){
-            
-            if ($password && (!preg_match('/[A-Z]/', $password) || !preg_match('/[0-9]/', $password) || !preg_match('/[!@#$%^&*(),.?":{}|<>]/', $password))) {
-                $errors['password'] = "Le mot de passe doit contenir au moins une lettre majuscule, un chiffre et un caractère spécial";
-            }
+        }else if($password && (!preg_match('/[A-Z]/', $password) || !preg_match('/[0-9]/', $password) || !preg_match('/[!@#$%^&*(),.?":{}|<>]/', $password))){
+            $errors['password'] = "Le mot de passe doit contenir au moins une lettre majuscule, un chiffre et un caractère spécial";
         }
 
-        if (!$pseudo || strlen($pseudo) < 3 || (!preg_match('/^[a-zA-Z]|[a-zA-Z0-9]*$/', $pseudo))) {
-             $errors['pseudo'] = "Le pseudo doit contenir au moins 3 caractères et ne doit contenir que des lettres et des chiffres.";
+        if (!$pseudo || strlen($pseudo) < 3  || strlen($pseudo) > 60 || (!preg_match('/^[a-zA-Z]|[a-zA-Z0-9]*$/', $pseudo))) {
+             $errors['pseudo'] = "Le pseudo doit contenir au moins 3 caractères et ne doit contenir que des lettres et des chiffres pour un max de 60 caractères.";
         }
 
-        if ($image['name']) {
+        if (isset($_FILES['profileImage']) && $_FILES['profileImage']['error'] === UPLOAD_ERR_OK) {
+            $image = $_FILES['profileImage'];
             $imageErrors = Utils::checkImage($image);
             if(!empty($imageErrors)){
                 $errors['image'] = $imageErrors;
             }
         }
+
+        
 
         $form = [
             'id' => $userId,
@@ -74,16 +77,186 @@ class UserController extends AbstractController
 
         $user = new User($form);
 
-        if (!empty($errors)) {
-            return $this->userProfile(true,$errors);
+        $isRegisterPage = $user->getId() == -1;
+     
+        /**Register New User */
+
+        if ($isRegisterPage) {
+              if(!empty($errors)){
+                $errors['lastInputs'] = [
+                    'pseudo' => $pseudo,
+                    'email' => $email,
+                    'password' => $password
+                ];
+                $_SESSION['form_errors'] = $errors;
+                $this->redirect("index.php?route=/register");
+            }
+
+            $userManager = new UserManager();
+            $result = $userManager->createOrUpdateUser($user);
+
+            if($result['success'] === true) {
+                unset($_SESSION["auth_token"]);
+                $this->redirect("index.php?route=/connection");
+            }else{
+                if ($result['email']) {
+                    $errors['email'] = "Ce mail existe deja";
+                }
+
+                if ($result['pseudo']) {
+                    $errors['pseudo'] = "Ce pseudo existe deja";
+                }
+                
+                if(!empty($errors)){
+                    $errors['lastInputs'] = [
+                    'pseudo' => $pseudo,
+                    'email' => $email,
+                    'password' => $password
+                    ];
+                    $_SESSION['form_errors'] = $errors;
+                    $this->redirect("index.php?route=/register");
+                }
+            }
+
+        /*Update User */
+
         }else{
-            $userManager = new UserManager(); 
-            
-            
-            $userManager->createOrUpdateUser($user,$lastImage,$image);
+            if(!Utils::checkCSRF("csrf-profile",$csrf)){
+                throw new Exception("Error Processing Request", 500);
+            }
+
+            $isValidUser = Utils::checkUser($userId);
+
+            if (!$isValidUser) {
+                 throw new Exception("Error Processing Request", 500);
+            }
+
+            if(!empty($errors)){
+                $errors['lastInputs'] = [
+                    'pseudo' => $pseudo,
+                    'email' => $email,
+                    'password' => $password
+                ];
+                $_SESSION['form_errors'] = $errors;
+                $this->redirect("index.php?route=/mon-compte");
+            }
+
+            $userManager = new UserManager();
+            $result = $userManager->createOrUpdateUser($user,$lastImage,$image);
+
+
+            if($result['success'] === true) {
+                $this->redirect("index.php?route=/mon-compte");
+            }else{
+                if ($result['email']) {
+                    $errors['email'] = "Ce mail existe deja";
+                }
+
+                if ($result['pseudo']) {
+                    $errors['pseudo'] = "Ce pseudo existe deja";
+                }
+                
+                if(!empty($errors)){
+                    $errors['lastInputs'] = [
+                    'pseudo' => $pseudo,
+                    'email' => $email,
+                    'password' => $password
+                    ];
+                    $_SESSION['form_errors'] = $errors;
+                    $this->redirect("index.php?route=/mon-compte");
+                }
+            }
         }
        
         $this->redirect("index.php?route=/mon-compte");
-       
     }
+
+
+
+    public function login(){
+        if ($_SERVER['REQUEST_METHOD'] === "POST") {
+            if ($_POST['submited'] === 'true') {
+                $errors = [];
+                $email = Utils::request("email","");
+                $password = Utils::request("password","");
+                $csrfToken = Utils::request("csrf","");
+
+                if (!$email || !filter_var($email,FILTER_VALIDATE_EMAIL)) {
+                    $errors['email'] = "Veuillez entrer une adresse email valide";
+                }
+
+                if (!$password) {
+                    $errors['password'] = "Veuillez entrer un mot de passe";
+                }
+
+                if (!Utils::checkCSRF("login_csrf",$csrfToken)) {
+                    throw new Exception("Error Processing Request", 500);
+                }
+            }else{
+                throw new Exception("Error Processing Request", 500);
+            }
+
+            if (!empty($errors)) {
+                $errors['lastInputs'] = [
+                    'email' => $email,
+                    'password' => $password
+                ];
+               $_SESSION['form_errors'] = $errors;
+               return $this->redirect("?route=/connection");
+            }
+
+            $userManager = new UserManager();
+            $data = $userManager->login($email,$password);
+
+            if ($data['user-error']) {
+                 $errors['lastInputs'] = [
+                    'email' => $email,
+                    'password' => $password
+                ];
+                $errors['user-error'] = $data['user-error'];
+                $_SESSION['form_errors'] = $errors;
+                return $this->redirect("?route=/connection");
+            }
+
+            if ($data['token']) {
+                $_SESSION['auth_token'] = $data['token'];
+                return $this->redirect("?route=/");
+            }
+
+            return $this->redirect("?route=/connection");
+        }
+
+        $title = "Connexion";
+
+        $csrf = bin2hex(random_bytes(16));
+
+        $_SESSION['login_csrf'] = $csrf;
+
+        $this->render("signup_signin",['title' =>$title, 'csrf' => $csrf, 'isConnectionPage' => true], $title);
+    }
+
+
+    
+    public function register(){
+        if ($_SERVER['REQUEST_METHOD'] === "POST") {
+
+            if ($_POST['submited'] === 'true') {
+                $csrfToken = Utils::request("csrf","");
+
+                if (!Utils::checkCSRF("register_csrf",$csrfToken)) {
+                    throw new Exception("Error Processing Request", 500);
+                }
+                
+                $this->createOrUpdateUser();
+            }else{
+                throw new Exception("Error Processing Request", 500);
+            }
+        }
+
+        $csrf = Utils::generateCSRF("register_csrf");
+        $title = "Inscription";
+        $this->render("signup_signin", ['title' =>$title, 'csrf' => $csrf, 'isConnectionPage' => false],$title);
+    }
+
+
 }
