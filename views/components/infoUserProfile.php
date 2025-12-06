5d97ef83667a2ca226fe4efedf0d9f81eac769ec
Marco <formante.marco@gmail.com>
2025-12-06 11:17:35 +0000

    handle input Errors

--- views/components/infoUserProfile.php
+++ views/components/infoUserProfile.php
@@ -1,3 +1,9 @@
+ <?php 
+    $errors = $_SESSION['form_errors'] ?? [];
+    $lastInputs = $errors['lastInputs'] ?? [];
+ ?>
+ 
+ 
  <form class="form-base"  method="post" enctype="multipart/form-data">
     <div class="user-box-container user-box-container-left">
         <div>
@@ -38,7 +44,7 @@
             <h4>Vos informations personnelles</h4>
             <div class="form-input-container" data-error="<?= isset($errors['email']) ? htmlspecialchars($errors['email']) : "" ?>">
                 <label for="email">Adresse email</label>
-                <input maxlength="60"  required  autocomplete="email" class="form-input__gray  <?= isset($errors['email']) ? "error-input-focus" : "" ?>"  type="email" name="email" id="email" value="<?= htmlspecialchars($user->getEmail()) ?>"/>
+                <input maxlength="60"  required  autocomplete="email" class="form-input__gray  <?= isset($errors['email']) ? "error-input-focus" : "" ?>"  type="email" name="email" id="email" value="<?= htmlspecialchars( $lastInputs['email'] ?? $user->getEmail()) ?>"/>
             </div>
             <div class="form-input-container"  data-error="<?= isset($errors['password']) ? htmlspecialchars($errors['password']) : "" ?>">
                 <label for="password">Mot de passe</label>
@@ -46,11 +52,19 @@
             </div>
             <div class="form-input-container" data-error="<?= isset($errors['pseudo']) ? htmlspecialchars($errors['pseudo']) : "" ?>">
                 <label for="pseudo">Pseudo</label>
-                <input maxlength="60" required class="form-input__gray  <?= isset($errors['pseudo']) ? "error-input-focus" : "" ?>"  type="text" name="pseudo" id="pseudo" value="<?= htmlspecialchars($user->getPseudo()) ?>"/>
+                <input maxlength="60" required class="form-input__gray  <?= isset($errors['pseudo']) ? "error-input-focus" : "" ?>"  type="text" name="pseudo" id="pseudo" value="<?= htmlspecialchars($lastInputs['pseudo'] ?? $user->getPseudo()) ?>"/>
             </div>
             <input hidden name="userId" value="<?= htmlspecialchars($user->getId()) ?>">
             <input hidden name="route" value="/updateUser">
-            <button class="btn-secondary" type="submit" value="submit">Enregistrer</button>
-        
+            <input hidden name="csrf" value="<?= htmlspecialchars($csrf) ?>">
+            
+            <?php if ($isOwner): ?>
+                   <button class="btn-secondary" type="submit" value="submit">Enregistrer</button>
+            <?php endif ?>
     </div>
 </form>
+
+
+<?php 
+
+unset($_SESSION['form_errors']);
