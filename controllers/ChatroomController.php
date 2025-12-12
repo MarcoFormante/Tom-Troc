8f0dfe056664d22eef0e3a39c5a814546720dda4
Marco <formante.marco@gmail.com>
2025-12-12 11:09:50 +0000

    add methods to verify chatrooms, edit showChatroom,show messages handle by messageController

--- controllers/ChatroomController.php
+++ controllers/ChatroomController.php
@@ -4,41 +4,62 @@ class ChatroomController extends AbstractController
 {
     public function showChatrooms(){
         $userId = Utils::checkUser();
-        $messages = [];
-        $otherUser = -1;
 
         if (!$userId) {
             $this->redirect("index.php?route=/connection");
         }
-        
+
         $chatroomManager = new ChatroomManager();
         $chatrooms = $chatroomManager->getUserChatrooms($userId);
 
-       
         $otherUserId = Utils::request("other_user_id",-1);
         $chatroomId = Utils::request("chatroom","");
 
         if (($otherUserId && is_numeric($otherUserId)) && $chatroomId) {
-            $userManager = new UserManager();
-            $otherUser = $userManager->getUser($otherUserId);
-            $messages = $this->getChatroomMessages($chatroomId);
-            return $this->render("chatrooms",['title' => "Messagerie","chatrooms" => $chatrooms,'messages' =>$messages,"userId" => $userId,"otherUser" => $otherUser],"Messagerie");
+            $messageController = new MessageController();
+            return $messageController->showMessages($chatrooms,$chatroomId,$otherUserId,$userId);
         }
         
         $this->render("chatrooms",['title' => "Messagerie","chatrooms" => $chatrooms,"userId" => $userId],"Messagerie");
     }
 
 
-    public function getChatroomMessages()
-    {
-        $chatroom_id = Utils::request("chatroom",-1);
 
-        if (!$chatroom_id) {
+
+    public function verifyChatroom(bool $isConnecting)
+    {
+        $otherUserId = Utils::request("other_user_id",-1);
+        $otherUserPseudo = Utils::request("pseudo","");
+
+        if (!$otherUserId || !is_numeric($otherUserId) || !$otherUserPseudo) {
             throw new Exception("Error Processing Request", 404);
         }
+        $userManager = new UserManager();
+        $user = $userManager->checkExistingUser($otherUserId,$otherUserPseudo);
 
-        $messageManager = new MessageManager();
-        $messages = $messageManager->getChatroomMessages($chatroom_id);
-        return $messages;
+        if (!$user) {
+            throw new Exception("Error Processing Request", 404);
+        }
+        if ($isConnecting) {
+            if($chatId = $this->checkExistingChatroomByUsers($otherUserId)){
+                unset($_SESSION['connectingWithUser']);
+                $this->redirect("index.php?route=/messages&chatroom=" . $chatId . "&other_user_id=" . $otherUserId);
+            }else{
+                $_SESSION['connectingWithUser'] = $user;
+                $this->redirect("index.php?route=/messages&connectingId=" . $user['id']);
+            }
+        }else{
+            return true;
+        }
+    }
+
+
+
+
+    private function checkExistingChatroomByUsers(int $otherUserId){
+        $userId = Utils::checkUser();
+
+        $chatroomManager = new ChatroomManager();
+        return $chatroomManager->checkExistingChatroomByUsers($userId,$otherUserId);
     }
 }
