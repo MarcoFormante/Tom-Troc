6ff44062a748b8824e81f4027802fdb3a62684af
Marco <formante.marco@gmail.com>
2025-12-12 11:05:39 +0000

    add scroll to bottom behavior for messages in chatroom

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -1,27 +1,36 @@
 
 
 document.addEventListener("DOMContentLoaded",()=>{
+    const messagesContainer = document.querySelector('.messages-container')
     const input = document.querySelector("input[type=file]")
     const image =  document.querySelector("img")
     if (input && image) {
             input.onchange = ()=>{
-            image.src = URL.createObjectURL(input.files[0]);
+            image.src = URL.createObjectURL(input.files[0])
         }
     }
 
-    const containers = document.querySelectorAll(".form-input-container, select");
+    const containers = document.querySelectorAll(".form-input-container, select")
 
     containers.forEach((container)=>{
-        const input = container.querySelector(".error-input-focus");
+        const input = container.querySelector(".error-input-focus")
         if (input) {
             input.oninput = ()=>{
                 if(input.classList.contains("error-input-focus")){
                     input.classList.remove("error-input-focus")
-                    container.dataset.error = ""
+                    container.dataset.error = "";
                 }
             }
         }
     })    
+
+
+    if (messagesContainer) {
+        messagesContainer.scrollTo({
+            "top":1000,
+            "behavior":"smooth"
+        });
+    }
 
 })
 
