75110398f39bac71969cb27295cf8bbd2009ec57
Marco <formante.marco@gmail.com>
2025-12-13 10:58:16 +0000

    send notification after message sent; redirect to chatrooms page if empty message; delete notification after show message relative of the notification; add comments

--- controllers/MessageController.php
+++ controllers/MessageController.php
@@ -2,6 +2,11 @@
 
 class MessageController extends AbstractController
 {
+    /**
+     * Send Message: handle new message for existing chatroom and for new chatroom;
+     * Send notification after sending message
+     * @return void
+     */
     public function sendMessage()
     {
         $otherUserId = Utils::request("other_user_id",-1);
@@ -12,7 +17,7 @@ class MessageController extends AbstractController
         $newConnection = Utils::request("connecting","") == "true" && $sentMessage && $_SESSION['connectingWithUser'] && Utils::request("submit","") == "true" && !$chatroomId;
 
         if (!$sentMessage) {
-            $this->redirect("index.php?route=/messages&chatroom=" . $chatroomId);
+              $this->redirect("index.php?route=/messages&chatroom=" . $chatroomId . "&other_user_id=" . $otherUserId);
         }
 
         $newMessage = new Message();
@@ -35,28 +40,57 @@ class MessageController extends AbstractController
         }
 
         $messageManager = new MessageManager();
-        $messageManager->sentMessage($newMessage);
+        $messageId = $messageManager->sentMessage($newMessage);
 
         if ($newConnection) {
             unset($_SESSION['connectingWithUser']);
         }
 
+        $notifController = new NotificationController();
+        $notifController->sendNotification($messageId,$userId,$otherUserId,$chatroomId);
+
         $this->redirect("index.php?route=/messages&chatroom=" . $chatroomId . "&other_user_id=" . $otherUserId);
     }
 
 
-
+    /**
+    * Retrieve messages for a specific chatroom, clear notifications once they are read, and render the chat view;
+    * Re-fetch notifications and store them in the session variable for further use.
+    *
+    * @param array $chatrooms An array containing all chatrooms associated with the user.
+    * @param string $chatroomId The current chatroom id to show
+    * @param int $otherUserId The ID of the other user in the chatroom
+    * @param int $userId The ID of the current user
+    * @return void
+    */
     public function showMessages(array $chatrooms,string $chatroomId,int $otherUserId,int $userId)
     {
         $userManager = new UserManager();
         $otherUser = $userManager->getUser($otherUserId);
         $messages = $this->getChatroomMessages($chatroomId,$otherUserId,$userId);
 
+        if ($notifications = $_SESSION['notifications']) {
+            foreach ($notifications as $notif) {
+                if ($notif['chatroom_id'] == $chatroomId) {
+                    $notifController = new NotificationController();
+                    $notifController->deleteNotification($chatroomId,$userId);
+                    $notifController->getNotifications();
+                }
+            }
+        }
+
         return $this->render("chatrooms", ['title' => "Messagerie", "chatrooms" => $chatrooms, 'messages' => $messages, "userId" => $userId, "otherUser" => $otherUser], "Messagerie");
     }
 
-
-    private function getChatroomMessages($chatroomId,$otherUserId,$userId)
+    /**
+     * Get all messages of a chatroom;
+     * Check if chatroom exists and retrieve all messages for the given users
+     * @param string $chatroomId Current chatroom id 
+     * @param int $otherUserId The ID of the other user in the chatroom 
+     * @param int $userId The ID of the current user
+     * @return array $messages
+    */
+    private function getChatroomMessages(string $chatroomId, int $otherUserId, int $userId)
     {
 
         if (!$chatroomId || !$otherUserId) {
