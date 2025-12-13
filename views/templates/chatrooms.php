086c66ac85bbde5ad6a48196694d60a0b91a74cc
Marco <formante.marco@gmail.com>
2025-12-13 11:27:50 +0000

    Retrieve notifications from session variable and get only chatroom ids to handle new messages; add comments and max-length till 255 to text input

--- views/templates/chatrooms.php
+++ views/templates/chatrooms.php
@@ -1,8 +1,18 @@
 
 <?php 
+    /** check if main user wants text to other user */
     $connectingUser = $_SESSION['connectingWithUser'] ?? null ;
-
+    /**Check if is the same user id of the query parameter  */
     $isValidConnectingUser = $connectingUser && Utils::request("connectingId") == $connectingUser['id'];
+    $notifications = $_SESSION['notifications'] ?? null;
+    $chatIdsNotifications = [];
+
+    /**Get only chatroom ids and put them in array (chatIdsNotifications) */
+    if ($notifications) {
+        foreach ($notifications as $key => $notif) {
+           $chatIdsNotifications[] =  $notif['chatroom_id'];
+        }
+    }
 ?>
 
 <div id="messagerie"> 
@@ -12,29 +22,33 @@
                 <h1><?= htmlspecialchars($title) ?></h1>
             </div>
            <ul class="chatrooms" aria-label="Liste de messages">
-                <?php foreach($chatrooms as $chatroom): ?>
-                    <li class="<?= Utils::request("chatroom",-1) == htmlspecialchars($chatroom['id'])? "selected" : "" ?>">
+                <?php foreach($chatrooms as $key => $chatroom): ?>
+                      <!-- Add class 'notificated' if user has some notication from this chatroom-->     
+                    <li class="<?= Utils::request("chatroom",-1) == htmlspecialchars($chatroom['id'])? "selected " : "" ?> <?= in_array($chatroom['id'],$chatIdsNotifications)   ? "notificated" : "" ?>">
                         <a href="index.php?route=/messages&chatroom=<?= htmlspecialchars($chatroom['id']) ?>&other_user_id=<?=  htmlspecialchars($chatroom['other_user_id']) ?>" >
                             <div class="message-info">
                                 <img class="user-img" src="<?= IMAGES_PATH . "users/" . htmlspecialchars($chatroom['other_user_image']) ?>" alt="<?= htmlspecialchars($chatroom['other_user_pseudo']) ?>, utilisateur du site TomTroc">
                                 <div class="user-info">
                                     <div class="color-dark-grey">
                                         <span class="user-pseudo"><?= htmlspecialchars($chatroom['other_user_pseudo']) ?></span> 
-                                        <time datetime="<?= date("c",strtotime(htmlspecialchars($chatroom['sent_at'])))?>"><?= date("H:i",date_create(htmlspecialchars($chatroom['sent_at']))->getTimestamp())?></time>
+                                        <time datetime="<?= date("c",strtotime(htmlspecialchars($chatroom['sent_at'])))?>"><?= Utils::formatDateTime(htmlspecialchars($chatroom['sent_at']))?></time>
                                     </div>
                                     <p class="last-message"><?= htmlspecialchars($chatroom['last_message']) ?></p>
                                 </div>
+                            </div>
+                            <div class="new-message-container">
+                                <p><?= in_array($chatroom['id'],$chatIdsNotifications) ? "Nouveau message" : "" ?></p>
                             </div>
                         </a>
                     </li>
                 <?php endforeach ?>
 
                 <?php if($connectingUser):?>
-                    <li class="selected">
+                    <li class="selected selected__grey">
                         <a href="index.php?route=/messages&connectingId=<?=  htmlspecialchars($connectingUser['id']) ?>"  >
                             <div class="message-info">
                                 <img class="user-img" src="<?= IMAGES_PATH . "users/" . htmlspecialchars($connectingUser['profile_image']) ?>" alt="<?= htmlspecialchars($connectingUser['pseudo']) ?>, utilisateur du site TomTroc">
-                                <div class="user-info">
+                                <div class="user-info self-center">
                                     <div class="color-dark-grey">
                                         <span class="user-pseudo"><?= htmlspecialchars($connectingUser['pseudo'])   ?></span> 
                                         <time></time>
@@ -72,7 +86,7 @@
                 <form method="post">
                     <div class="message-input">
                         <label hidden for="message">Message</label>
-                        <input required type="text" id="message" name="message" placeholder="Tapez votre message ici"/>
+                        <input maxlength="255" required type="text" id="message" name="message" placeholder="Tapez votre message ici"/>
                         <input hidden required name="route" value="/sendMessage"/>
                     </div>
                     <div>
@@ -98,7 +112,7 @@
                 <form method="post">
                     <div class="message-input">
                         <label hidden for="message">Message</label>
-                        <input required type="text" id="message" name="message" placeholder="Tapez votre message ici"/>
+                        <input maxlength="255" required type="text" id="message" name="message" placeholder="Tapez votre message ici" />
                         <input hidden required name="connecting" value="true"/>
                         <input hidden required name="route" value="/sendMessage"/>
                     </div>
