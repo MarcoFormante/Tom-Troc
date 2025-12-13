4d0a22b40200cf85e7150145ee9d17345d980d43
Marco <formante.marco@gmail.com>
2025-12-13 11:25:25 +0000

    correct html lang in 'fr' ; get Notifications every time user refreshes or changes the page

--- views/layouts/main_layout.php
+++ views/layouts/main_layout.php
@@ -1,5 +1,12 @@
+<?php 
+/** Get Notifications */
+$notificationController = new NotificationController();
+$notifications = $notificationController->getNotifications();
+?>
+
+
 <!DOCTYPE html>
-<html lang="en">
+<html lang="fr">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
