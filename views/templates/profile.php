5c7ffb7a53951ab98c50e7df54156b9b8c1660a1
Marco <formante.marco@gmail.com>
2025-12-18 20:22:25 +0000

    add elements for mobile screen

--- views/templates/profile.php
+++ views/templates/profile.php
@@ -3,7 +3,6 @@
     <h1 class="playfair-display-400"> <?=!$isOwner ?"Compte de " . htmlspecialchars($user->getPseudo()) : "Mon compte" ?> </h1>
     <div id="user-profile-flex-container" class="<?= !$isOwner ? "flex-row" : "" ?>">
         <section id="user-profile-info-section">
-
             <?php if($isOwner): ?>  
                 <?php require("../views/components/infoUserProfile.php")?>
             <?php else: ?>
@@ -73,5 +72,43 @@
                 </p>
             <?php endif ?>
         </section>
+  
+        <section class="hide-tablet" id="user-books-mobile-container">
+                <ul aria-label="list des livres">
+                    <?php foreach($books as $book):?>
+                        <li class="<?= $isOwner ? "book-card-user" : "book-card-public" ?>">
+                            <div class="book-info-top">
+                                <div class="book-info-top-container">
+                                    <picture>
+                                       <img  width="79px" height="79px" src="<?= IMAGES_PATH . 'books/' . htmlspecialchars($book->getImage()) ?>" alt="<?= htmlspecialchars($book->getTitle()) . " de " . htmlspecialchars($book->getAuthor())?>">
+                                    </picture>
+                                    <div class="book-info-top-text">
+                                        <span class="book-info-title"> <?= htmlspecialchars($book->getTitle()) ?></span>
+                                        <span class="book-info-author"> <?= htmlspecialchars($book->getAuthor()) ?></span>
+                                        <div class="book-status <?= $book->getStatus() ? "book-status__green" : "book-status__red"  ?>">
+                                            <?= $book->getStatus() ? "disponible" : "non dispo." ?>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="book-info-bottom">
+                                <p><?= htmlspecialchars(substr_replace($book->getDescription(),"...",70)) ?></p>
+                            </div>
+                        <?php if($isOwner): ?>
+                            <div class="book-card-user-btns">
+                                <a href="?route=/editBook&book_id=<?= htmlspecialchars($book->getId()) ?>&sold_by=<?= htmlspecialchars($book->getSoldBy()) ?>">Éditer</a>
+                                <form class="table-form" method="post">
+                                    <input hidden name="route" value="/deleteBook"/>
+                                    <input hidden type="text" name="bookId" value="<?= htmlspecialchars($book->getId()) ?>"/>
+                                    <input hidden type="text" name="soldBy" value="<?= htmlspecialchars($book->getSoldBy()) ?>"/>
+                                    <button class="btn-text" type="submit" name="submit" value="submited">Supprimer</button>
+                                </form>
+                            </div>
+                        <?php endif ?>
+                        </li>  
+                    <?php endforeach ?>
+                </ul>
+        </section>
+        
     </div>
 </div>
