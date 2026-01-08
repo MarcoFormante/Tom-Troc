0bb9c10c756340ce13adec88516c5a6258ebe367
Marco <formante.marco@gmail.com>
2026-01-08 23:53:45 +0000

    add h2 instead of h4 tag

--- views/components/book.php
+++ views/components/book.php
@@ -3,7 +3,7 @@
         <div class="book-card-white-container">
             <div class="flex-col-gap-16">
                 <img  width="200" height="200" src="<?= IMAGES_PATH . 'books/' . htmlspecialchars($book->getImage()) ?>" alt="<?=  htmlspecialchars($book->getTitle()) ?>">
-                <h4 class="book-card-title"><?= htmlspecialchars($book->getTitle()) ?></h4>
+                <h2 class="book-card-title"><?= htmlspecialchars($book->getTitle()) ?></h2>
                 <span class="book-card-author"><?= htmlspecialchars($book->getAuthor())?></span>
             </div>
             <p class="book-card-soldby">Vendu par : <?= htmlspecialchars($book->getUser()->getPseudo())?></p>
