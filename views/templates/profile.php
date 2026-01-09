0dfecb7307962a69b40be268913bd7b2346fcb65
Marco <formante.marco@gmail.com>
2026-01-09 00:11:03 +0000

    add aria attributes and adjust the table

--- views/templates/profile.php
+++ views/templates/profile.php
@@ -1,4 +1,14 @@
 
+<?php 
+    $redirect = Utils::request("redirect","");
+    $redirectBookId = Utils::request("bookId","");
+    $retourHref = "index.php?route=" . $redirect . "&id=" .$redirectBookId;
+?>
+
+<?php if(!$isOwner && ($redirect && $redirectBookId)): ?>
+    <a class="retour m-20 block pl-150" href="<?= $retourHref ?>"><- retour</a>
+<?php endif ?>
+
 <div id="user-profile"  class="<?= !$isOwner ? "user-profile-public" : "" ?>">
     <h1 class="playfair-display-400"> <?=!$isOwner ?"Compte de " . htmlspecialchars($user->getPseudo()) : "Mon compte" ?> </h1>
     <div id="user-profile-flex-container" class="<?= !$isOwner ? "flex-row" : "" ?>">
@@ -14,50 +24,46 @@
             <table>
                 <thead>
                     <tr>
-                        <th class="th-spacing"></th>
                         <th scope="col">PHOTO</th>
-                        <th scope="col" class="p-left">TITRE</th>
-                        <th scope="col" class="p-left">AUTEUR</th>
-                        <th scope="col" class="desc-spacing">DESCRIPTION</th>
+                        <th scope="col">TITRE</th>
+                        <th scope="col">AUTEUR</th>
+                        <th scope="col">DESCRIPTION</th>
                     <?php if($isOwner):?>  
-                        <th scope="col" class="p-left">DISPONIBILITE</th>
-                        <th scope="col" class="edit-spacing" class="p-left">ACTION</th>
-                        <th scope="col"></th>
+                        <th  scope="col">DISPONIBILITE</th>
+                        <th scope="col">ACTION</th>
                     <?php endif ?>
                     </tr>
                 </thead>
                 <tbody>
-                    <tr class="body-spacing"><td></td></tr>
                     <?php foreach($books as $book):?>
                         <tr>
-                            <td></td>
-                            <td class="w-127">
-                                <img  width="78px" height="78px" src="<?= IMAGES_PATH . 'books/' . htmlspecialchars($book->getImage()) ?>" alt="<?= htmlspecialchars($book->getTitle()) . " de " . htmlspecialchars($book->getAuthor())?>">
+                            <td>
+                                <img width="78" height="78" src="<?= IMAGES_PATH . 'books/' . htmlspecialchars($book->getImage()) ?>" alt="<?= htmlspecialchars($book->getTitle()) . " de " . htmlspecialchars($book->getAuthor())?>">
                             </td>
-                            <td class="w-155 p-left">
+                            <td>
                                 <?= htmlspecialchars($book->getTitle()) ?>
                             </td>
-                            <td class="w-138 p-left">
+                            <td>
                                 <?= htmlspecialchars($book->getAuthor()) ?>
                             </td>
-                            <td class="w-146 desc-spacing p-left">
+                            <td>
                                 <p><?= htmlspecialchars(substr_replace($book->getDescription(),"...",70)) ?></p>
                             </td>
 
                             <?php if($isOwner):?>    
-                                <td class="w-81 p-left">
+                                <td class="w-81 ">
                                     <div class="book-status <?= $book->getStatus() ? "book-status__green" : "book-status__red"  ?>">
                                         <?= $book->getStatus() ? "disponible" : "non dispo." ?>
                                     </div>
                                 </td>
-                                <td >
+                                <td>
                                     <a href="?route=/editBook&book_id=<?= htmlspecialchars($book->getId()) ?>&sold_by=<?= htmlspecialchars($book->getSoldBy()) ?>">Éditer</a>
                                 </td>
-                                <td class="w-127">
+                                <td class="pl-28    ">
                                    <form class="table-form" method="post">
-                                        <input hidden name="route" value="/deleteBook"/>
-                                        <input hidden type="text" name="bookId" value="<?= htmlspecialchars($book->getId()) ?>"/>
-                                        <input hidden type="text" name="soldBy" value="<?= htmlspecialchars($book->getSoldBy()) ?>"/>
+                                        <input type="hidden"  aria-hidden="true" hidden name="route" value="/deleteBook"/>
+                                        <input type="hidden"  aria-hidden="true" hidden type="text" name="bookId" value="<?= htmlspecialchars($book->getId()) ?>"/>
+                                        <input type="hidden"  aria-hidden="true" hidden type="text" name="soldBy" value="<?= htmlspecialchars($book->getSoldBy()) ?>"/>
                                         <button class="btn-text" type="submit" name="submit" value="submited">Supprimer</button>
                                     </form>
                                 </td>
@@ -80,7 +86,7 @@
                             <div class="book-info-top">
                                 <div class="book-info-top-container">
                                     <picture>
-                                       <img  width="79px" height="79px" src="<?= IMAGES_PATH . 'books/' . htmlspecialchars($book->getImage()) ?>" alt="<?= htmlspecialchars($book->getTitle()) . " de " . htmlspecialchars($book->getAuthor())?>">
+                                       <img  width="79" height="79" src="<?= IMAGES_PATH . 'books/' . htmlspecialchars($book->getImage()) ?>" alt="<?= htmlspecialchars($book->getTitle()) . " de " . htmlspecialchars($book->getAuthor())?>">
                                     </picture>
                                     <div class="book-info-top-text">
                                         <span class="book-info-title"> <?= htmlspecialchars($book->getTitle()) ?></span>
@@ -98,9 +104,9 @@
                             <div class="book-card-user-btns">
                                 <a href="?route=/editBook&book_id=<?= htmlspecialchars($book->getId()) ?>&sold_by=<?= htmlspecialchars($book->getSoldBy()) ?>">Éditer</a>
                                 <form class="table-form" method="post">
-                                    <input hidden name="route" value="/deleteBook"/>
-                                    <input hidden type="text" name="bookId" value="<?= htmlspecialchars($book->getId()) ?>"/>
-                                    <input hidden type="text" name="soldBy" value="<?= htmlspecialchars($book->getSoldBy()) ?>"/>
+                                    <input type="hidden" aria-hidden="true" hidden name="route" value="/deleteBook"/>
+                                    <input type="hidden"  aria-hidden="true" hidden name="bookId" value="<?= htmlspecialchars($book->getId()) ?>"/>
+                                    <input type="hidden"  aria-hidden="true" hidden name="soldBy" value="<?= htmlspecialchars($book->getSoldBy()) ?>"/>
                                     <button class="btn-text" type="submit" name="submit" value="submited">Supprimer</button>
                                 </form>
                             </div>
